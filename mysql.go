17efec38149aed8b333c8c70aace768f3d820ef9
zeeraw <zee@vall.in>
2019-04-10 17:29:37 +0200

    feat: provide mysql specific functions for deriving its url from env

---
+++ mysql.go
@@ -0,0 +1,17 @@
+package coresql
+
+import "os"
+
+const (
+	// DefaultMySQLURL is the default url to a MYSQL database.
+	DefaultMySQLURL = "mysql://127.0.0.1:3306/service"
+)
+
+// MySQLURLFromEnv tries to retrieve the redis url from the environment.
+func MySQLURLFromEnv() string {
+	url := os.Getenv("MYSQL_URL")
+	if url == "" {
+		url = DefaultMySQLURL
+	}
+	return url
+}
