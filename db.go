9b9d00ae75c5ae522b1788002407dbe02f2660f6
zeeraw <zee@vall.in>
2019-04-11 17:49:44 +0200

    feat: provide methods for deriving migrations from core-sql

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/golang-migrate/migrate"
 	"github.com/xo/dburl"
 )
 
@@ -12,28 +13,50 @@ type DB struct {
 	*sql.DB
 }
 
-// Open will attempt to open a new database connection.
-func Open(u string) (*DB, error) {
-	db, err := dburl.Open(u)
-	if err != nil {
-		return nil, err
-	}
-	return &DB{db}, nil
-}
-
-// MustOpen will crash your program unless a database could be retrieved.
-func MustOpen(u string) *DB {
-	db, err := Open(u)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return db
-}
-
 // Check will attempt to ping the database to see if the connection is still alive.
 func (db *DB) Check() ([]string, bool) {
 	if err := db.Ping(); err != nil {
 		return []string{err.Error()}, false
 	}
 	return []string{"database connection ok"}, true
+}
+
+// Open will attempt to open a new database connection.
+func Open(databaseURL string) (*DB, error) {
+	database, err := dburl.Open(databaseURL)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{database}, nil
+}
+
+// MustOpen will crash your program unless a database could be retrieved.
+func MustOpen(databaseURL string) *DB {
+	db, err := Open(databaseURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
+// OpenWithMigrations opens a database connection with an associated migration instance.
+func OpenWithMigrations(databaseURL, migrationsURL string) (*DB, *migrate.Migrate, error) {
+	migration, err := migrate.New(migrationsURL, databaseURL)
+	if err != nil {
+		return nil, nil, err
+	}
+	database, err := Open(databaseURL)
+	if err != nil {
+		return nil, nil, err
+	}
+	return database, migration, nil
+}
+
+// MustOpenWithMigrations opens a database connection with an associated migration instance and crashes if unsuccessful.
+func MustOpenWithMigrations(databaseURL, migrationsURL string) (*DB, *migrate.Migrate) {
+	database, migrations, err := OpenWithMigrations(databaseURL, migrationsURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return database, migrations
 }
