f23e925e64a312c7b3ceec0597ced0140f4e6671
zeeraw <zee@vall.in>
2019-04-11 20:29:45 +0200

    fix: migrate helpers now actually work :poop:

--- migrate.go
+++ migrate.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"os"
+
+	"github.com/golang-migrate/migrate/v4"
 )
 
 // Migrate represents functionality for
@@ -13,20 +15,21 @@ type Migrate interface {
 }
 
 // HandleMigrationArgs should be invoked to handle command line arguments for running migrations.
-func HandleMigrationArgs(migrate Migrate) {
-	switch flag.Arg(1) {
+func HandleMigrationArgs(mig Migrate) {
+	flag.Parse()
+	switch flag.Arg(0) {
 	case "migrate":
-		switch flag.Arg(2) {
+		switch flag.Arg(1) {
 		case "up":
-			if err := migrate.Up(); err != nil {
+			if err := mig.Up(); err != nil {
 				log.Fatalln(err)
 			}
 		case "down":
-			if err := migrate.Down(); err != nil {
+			if err := mig.Down(); err != nil {
 				log.Fatalln(err)
 			}
 		default:
-			log.Fatalln("need to provide a command to 'migrate'")
+			log.Fatalln("migrate needs a sub-command")
 		}
 		os.Exit(0)
 		return
@@ -34,8 +37,13 @@ func HandleMigrationArgs(migrate Migrate) {
 }
 
 // MustMigrateUp will attempt to migrate your database up.
-func MustMigrateUp(migrate Migrate) {
-	if err := migrate.Up(); err != nil {
-		log.Fatalln(err)
+func MustMigrateUp(mig Migrate) {
+	if err := mig.Up(); err != nil {
+		switch err {
+		case migrate.ErrNoChange:
+			log.Println("database migration:", "no changes")
+		default:
+			log.Fatalln("database migration:", err)
+		}
 	}
 }
